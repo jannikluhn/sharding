db1aacf071b1f9d56f88bc69232598522a2f0da9
Jannik Luhn <jannik.luhn@brainbot.com>
2017-11-26 22:50:40 +0800

    Replace pyethereum with pyevm in collation.py

--- sharding/tests/test_collation.py
+++ sharding/tests/test_collation.py
@@ -1,13 +1,12 @@
-from ethereum.utils import encode_hex
 from sharding.collation import (
     CollationHeader,
     Collation,
 )
+from eth_utils import is_same_address
+from evm.rlp.transactions import BaseTransaction
 
 
-def test_collation_init():
-    """Test Collation initialization
-    """
+def test_collation():
     coinbase = '\x35' * 20
 
     collation_header = CollationHeader(coinbase=coinbase)
@@ -15,5 +14,22 @@ def test_collation_init():
     collation = Collation(collation_header)
     collation_header_dict = collation.header.to_dict()
 
+    assert collation.header == collation_header
+    assert collation.transactions == []
     assert collation.transaction_count == 0
-    assert collation_header_dict['coinbase'] == encode_hex(coinbase)
+    assert is_same_address(collation_header_dict['coinbase'], coinbase)
+
+    tx = BaseTransaction(
+        nonce=0,
+        gas_price=0,
+        gas=0,
+        to='\xff' * 20,
+        value=0,
+        data='',
+        v=0,
+        r=0,
+        s=0,
+    )
+    collation.transactions.append(tx)
+    assert collation.transactions == [tx]
+    assert collation.transaction_count == 1
